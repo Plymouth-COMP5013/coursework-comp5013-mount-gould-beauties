27eadd488b4817efcf7439948d82ae3e79c5a159
ReefLakin <reeflakin2@gmail.com>
2025-04-30 14:17:16 +0100

    ✨ : STGCN Model Improvements
    Made a couple little changes to the STGCN model to improve readability and just make it work better.

--- stgcn.py
+++ stgcn.py
@@ -4,11 +4,13 @@
 # Helpful Links:
 #   https://github.com/benedekrozemberczki/pytorch_geometric_temporal/blob/master/torch_geometric_temporal/nn/attention/stgcn.py
 #   https://github.com/benedekrozemberczki/pytorch_geometric_temporal
+#   https://pytorch-geometric-temporal.readthedocs.io/en/latest/notes/introduction.html#epidemiological-forecasting
 
 # Imports
 import math
 import torch
 import torch.nn as nn
+from torch.nn import Linear
 import torch.nn.functional as F
 from torch_geometric_temporal.nn.attention import STConv
 
@@ -24,56 +26,22 @@ class STGCN(torch.nn.Module):
         out_channels (int): Number of output channels.
         num_nodes (int): Number of nodes in the graph.
         kernel_size (int, optional): Size of the convolutional kernel. Default is 3.
+        K (int, optional): Order of Chebyshev polynomial. Default is 3.
     """
 
 
-    def __init__(self, in_channels, hidden_channels, out_channels, num_nodes, kernel_size=3):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_nodes, kernel_size=3, K=3):
         super(STGCN, self).__init__()
 
-        # The first spatio-temporal convolutional layer
-        self.st_conv1 = STConv(
-            num_nodes=num_nodes,
-            in_channels=in_channels,
-            hidden_channels=hidden_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            K=3,  # order of Chebyshev polynomial
-            normalization="sym"
-        )
+        self.stconv1 = STConv(num_nodes, in_channels, hidden_channels, hidden_channels, kernel_size, K)
+        self.stconv2 = STConv(num_nodes, hidden_channels, hidden_channels, hidden_channels, kernel_size, K)
+        self.final_linear = Linear(hidden_channels, out_channels)
 
-        # The second spatio-temporal convolutional layer
-        self.st_conv2 = STConv(
-            num_nodes=num_nodes,
-            in_channels=in_channels,
-            hidden_channels=hidden_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            K=3,  # order of Chebyshev polynomial
-            normalization="sym"
-        )
-
-        # Final convolutional layer to reduce to output features
-        self.final_conv = torch.nn.Conv2d(
-            in_channels=out_channels,
-            out_channels=out_channels,
-            kernel_size=(1, 1)
-        )
-
-        self.fc = torch.nn.Linear(num_nodes * out_channels, num_nodes)
-
-    def forward(self, x, edge_index):
-        # x shape: [batch_size, num_nodes, num_features, sequence_length]
-        x = self.st_conv1(x, edge_index)
+    def forward(self, x, edge_index, edge_weight=None):
+        x = self.stconv1(x, edge_index, edge_weight)
         x = F.relu(x)
-
-        x = self.st_conv2(x, edge_index)
+        x = self.stconv2(x, edge_index, edge_weight)
         x = F.relu(x)
-
-        # Final conv to reduce to output features
-        x = self.final_conv(x.unsqueeze(1))  # Add channel dimension: [B, 1, N, F, T]
-        x = x.squeeze(1)  # Remove channel dimension
-
-        # Flatten and apply FC layer
-        x = x.view(x.shape[0], -1)
-        x = self.fc(x)
+        x = x[:, -1]  # Take the last time step
+        x = self.final_linear(x)
         return x
